d1a0ea770dd665c40cab4fb9afe90b5665a38e2e
volodymyr-miretskyi <vova.miretskyi@gmail.com>
2025-05-31 20:48:11 +0300

    app: added infrastructure

---
+++ src/server/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+import router from "./router";
+
+const app = express();
+
+app.use(express.static("./src/client"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/api", router);
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
